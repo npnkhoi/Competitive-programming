c182de53e0c7c74a665a7c305befa2438faa2654
npnkhoi <npnkhoi@gmail.com>
2021-11-22 10:40:36 +0700

    update

--- VNOI/M.cpp
+++ VNOI/M.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main() {
-  freopen("test.inp", "r", stdin);
-  freopen("test.out", "w", stdout);
+  // freopen("test.inp", "r", stdin);
+  // freopen("test.out", "w", stdout);
   double e, w, s;
   cin >> e >> w >> s;
   cout << fixed << setprecision(10);
